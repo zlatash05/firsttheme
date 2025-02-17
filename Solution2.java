61a9e3917beed51f7e744bb7a5bb1268bfaa6f3a
zlatash05 <shkabara.zlata.2005@gmail.com>
2025-02-17 17:02:11 +0500

    Update Solution2.java

--- Solution2.java
+++ Solution2.java
@@ -1,4 +1,4 @@
-class Solution2 {
+class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
         ListNode current = dummy;
