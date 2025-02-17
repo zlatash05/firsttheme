01d171efa7bfa4b71e7471523d2a2ac64ddf1884
zlatash05 <shkabara.zlata.2005@gmail.com>
2025-02-17 17:01:09 +0500

    Add files via upload

---
+++ Solution5.java
@@ -0,0 +1,29 @@
+
+class Solution5 {
+        public ListNode rotateRight(ListNode head, int k) {
+            if (head == null || head.next == null || k == 0) {
+                return head;
+            }
+
+            ListNode tail = head;
+            int size = 1;
+
+            while (tail.next != null) {
+                size++;
+                tail = tail.next;
+            }
+
+            int rotationCount = k % size;
+            tail.next = head;
+
+            for (int i = 0; i < size - rotationCount; i++) {
+                tail = tail.next;
+            }
+
+
+            head = tail.next;
+            tail.next = null;
+
+            return head;
+        }
+    }
---
+++ Solution6.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution6 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>(i + 1);
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);  // The first and last elements are always 1
+                } else {
+                    row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
+
+
